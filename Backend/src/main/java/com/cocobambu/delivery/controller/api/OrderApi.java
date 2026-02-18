26553c35210e95cfd5b30779c8ce0f783185f730
Matheus Cunha <matheuscunhaprado@gmail.com>
2026-02-17 21:16:03 -0300

    docs: consolidar descrição de erro 400 na documentação da API

--- Backend/src/main/java/com/cocobambu/delivery/controller/api/OrderApi.java
+++ Backend/src/main/java/com/cocobambu/delivery/controller/api/OrderApi.java
@@ -22,8 +22,7 @@ public interface OrderApi {
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Pedido criado com sucesso", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Erro de validação (campos inválidos)", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Erro de mismatch de valor pago", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Erro de validação (campos inválidos) ou erro de mismatch de valor pago", content = @Content),
             @ApiResponse(responseCode = "404", description = "Erro, não foi possível encontrar loja com o id fornecido", content = @Content),
             @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
     })
